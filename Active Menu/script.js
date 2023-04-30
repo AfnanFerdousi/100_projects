86bd30a4f130d3e96931bb8af0084247a34c70e2
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-04-30 18:28:40 +0600

    push

--- Active Menu/script.js
+++ Active Menu/script.js
@@ -1,11 +1,10 @@
 // Active Menu Switcher
-
 const navList = document.querySelector(".nav-list");
 
 navList.addEventListener("click", (e) => {
   const navLink = e.target.parentElement;
-  if (navLink.classList.contains("link")) {
-    navList.querySelector(".active").classList.remove("active");
-    navLink.classList.add("active");
+  if(navLink.classList.contains("link")){
+    navList.querySelector(".active").classList.remove("active")
+    navLink.classList.add("active")
   }
-});
+})
